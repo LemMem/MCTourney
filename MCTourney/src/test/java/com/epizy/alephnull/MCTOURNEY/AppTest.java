067cb99f263bbff61dbaf89bd4a91b641c3f486e
Aleph <liammelnicck@gmail.com>
2019-11-15 22:08:13 -0500

    Setup Maven Project
    The name is all I did

---
+++ MCTourney/src/test/java/com/epizy/alephnull/MCTOURNEY/AppTest.java
@@ -0,0 +1,20 @@
+package com.epizy.alephnull.MCTOURNEY;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
